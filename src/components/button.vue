f1171b80ecf42405c24a1643835d7f07de64fcc0
trass-ruku <sekigcuhi@trass.co.jp>
2025-05-02 16:12:32 +0900

    ルーティングさせて作ろうとしています

---
+++ src/components/button.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template >
+    <button id="button-design">テスト作成</button>
+</template>
+
+<style>
+#button-design {
+    margin: 0 auto;
+    border-radius: 2em;
+    border-color: chocolate;
+    padding: 10px;
+    background-color: chocolate;
+    color: white;
+}
+</style>
