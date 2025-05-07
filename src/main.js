418cc65c6fcdcc1b4b1a0dc68ad3791ac3c9e7bb
trass-ruku <sekigcuhi@trass.co.jp>
2025-05-07 15:40:06 +0900

    bootstrapパッケージ追加

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-// import './assets/style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
 createApp(App).mount('#app')
